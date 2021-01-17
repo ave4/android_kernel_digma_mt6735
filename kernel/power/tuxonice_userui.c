98d8ac3b12eee24a33a9225f54ca0f258fbfa6ae
Михалыч <gtk3@inbox.ru>
2021-01-17 21:46:08 +0500

    02_fix_various_include_directives_mt6735

--- kernel/power/tuxonice_userui.c
+++ kernel/power/tuxonice_userui.c
@@ -315,10 +315,10 @@ static void userui_prepare_status(int clearbar, const char *fmt, ...)
 	if (clearbar)
 		toi_update_status(0, 1, NULL);
 
-	if (ui_helper_data.pid == -1)
+	//if (ui_helper_data.pid == -1)
 		pr_err("%s\n", lastheader);
-	else
-		toi_message(0, TOI_STATUS, 1, lastheader, NULL);
+	//else
+	//	toi_message(0, TOI_STATUS, 1, lastheader, NULL);
 }
 
 /**
